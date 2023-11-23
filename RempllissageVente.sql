42d1f03e35c9ca1c5c124ee068782e59c6b6707c
Mohamad Satea ALMALLOUHI <75503482+SateaMall@users.noreply.github.com>
2023-11-23 19:39:21 +0100

    Update RempllissageVente.sql

--- RempllissageVente.sql
+++ RempllissageVente.sql
@@ -1,1 +1,28 @@
+----- Employe -----
+INSERT INTO Employe VALUES (3222, 'Martin', 'Dupont', 'CHEF', TO_DATE('01-01-2015', 'DD-MM-YYYY'), 4500.00, 5, 35, 'martin.dupont@email.com', TO_DATE('15-03-1985', 'DD-MM-YYYY'));
+INSERT INTO Employe VALUES (3223, 'Lucie', 'Moreau', 'MANAGER', TO_DATE('15-06-2016', 'DD-MM-YYYY'), 4000.00, 3, 35, 'lucie.moreau@email.com', TO_DATE('22-07-1987', 'DD-MM-YYYY'));
+INSERT INTO Employe VALUES (3224, 'Maxime', 'Bernard', 'POLYVALENT', TO_DATE('10-09-2017', 'DD-MM-YYYY'), 3500.00, 2, 30, 'maxime.bernard@email.com', TO_DATE('08-11-1989', 'DD-MM-YYYY'));
+INSERT INTO Employe VALUES (3225, 'Sophie', 'Petit', 'CHEF', TO_DATE('03-03-2018', 'DD-MM-YYYY'), 4700.00, 7, 40, 'sophie.petit@email.com', TO_DATE('14-02-1986', 'DD-MM-YYYY'));
+INSERT INTO Employe VALUES (3226, 'Paul', 'Roux', 'MANAGER', TO_DATE('17-07-2019', 'DD-MM-YYYY'), 4200.00, 4, 35, 'paul.roux@email.com', TO_DATE('09-05-1988', 'DD-MM-YYYY'));
+INSERT INTO Employe VALUES (3227, 'Julie', 'Vincent', 'POLYVALENT', TO_DATE('20-11-2020', 'DD-MM-YYYY'), 3400.00, 1, 25, 'julie.vincent@email.com', TO_DATE('23-08-1990', 'DD-MM-YYYY'));
+INSERT INTO Employe VALUES (3228, 'Pierre', 'David', 'CHEF', TO_DATE('05-05-2021', 'DD-MM-YYYY'), 4600.00, 6, 38, 'pierre.david@email.com', TO_DATE('16-01-1984', 'DD-MM-YYYY'));
+INSERT INTO Employe VALUES (3229, 'Marie', 'Leroy', 'MANAGER', TO_DATE('12-10-2022', 'DD-MM-YYYY'), 4100.00, 5, 35, 'marie.leroy@email.com', TO_DATE('29-04-1987', 'DD-MM-YYYY'));
+INSERT INTO Employe VALUES (3230, 'Nicolas', 'Simon', 'POLYVALENT', TO_DATE('07-08-2023', 'DD-MM-YYYY'), 3300.00, 2, 28, 'nicolas.simon@email.com', TO_DATE('05-06-1991', 'DD-MM-YYYY'));
+INSERT INTO Employe VALUES (3231, 'Camille', 'Laurent', 'CHEF', TO_DATE('18-01-2014', 'DD-MM-YYYY'), 4800.00, 7, 40, 'camille.laurent@email.com', TO_DATE('20-12-1985', 'DD-MM-YYYY'));
+INSERT INTO Employe VALUES (3232, 'Thomas', 'Martinez', 'MANAGER', TO_DATE('21-04-2015', 'DD-MM-YYYY'), 3900.00, 3, 35, 'thomas.martinez@email.com', TO_DATE('31-10-1988', 'DD-MM-YYYY'));
+INSERT INTO Employe VALUES (3233, 'Anne', 'Garcia', 'POLYVALENT', TO_DATE('24-07-2016', 'DD-MM-YYYY'), 3600.00, 1, 30, 'anne.garcia@email.com', TO_DATE('13-09-1989', 'DD-MM-YYYY'));
+INSERT INTO Employe VALUES (3234, 'François', 'Lefebvre', 'CHEF', TO_DATE('27-11-2017', 'DD-MM-YYYY'), 4500.00, 6, 38, 'francois.lefebvre@email.com', TO_DATE('17-03-1986', 'DD-MM-YYYY'));
+INSERT INTO Employe VALUES (3235, 'Charlotte', 'Perrin', 'MANAGER', TO_DATE('02-03-2018', 'DD-MM-YYYY'), 4000.00, 4, 35, 'charlotte.perrin@email.com', TO_DATE('26-05-1987', 'DD-MM-YYYY'));
+INSERT INTO Employe VALUES (3236, 'Alexandre', 'Dubois', 'POLYVALENT', TO_DATE('06-08-2019', 'DD-MM-YYYY'), 3500.00, 2, 32, 'alexandre.dubois@email.com', TO_DATE('30-07-1990', 'DD-MM-YYYY'));
+INSERT INTO Employe VALUES (3237, 'Catherine', 'Durand', 'CHEF', TO_DATE('10-12-2020', 'DD-MM-YYYY'), 4600.00, 5, 40, 'catherine.durand@email.com', TO_DATE('04-02-1985', 'DD-MM-YYYY'));
+INSERT INTO Employe VALUES (3238, 'Guillaume', 'Robert', 'MANAGER', TO_DATE('15-04-2021', 'DD-MM-YYYY'), 4200.00, 3, 35, 'guillaume.robert@email.com', TO_DATE('11-08-1988', 'DD-MM-YYYY'));
+INSERT INTO Employe VALUES (3239, 'Elodie', 'Morel', 'POLYVALENT', TO_DATE('19-09-2022', 'DD-MM-YYYY'), 3300.00, 1, 28, 'elodie.morel@email.com', TO_DATE('18-10-1989', 'DD-MM-YYYY'));
+INSERT INTO Employe VALUES (3240, 'Vincent', 'Richard', 'CHEF', TO_DATE('23-01-2023', 'DD-MM-YYYY'), 4700.00, 7, 38, 'vincent.richard@email.com', TO_DATE('09-01-1984', 'DD-MM-YYYY'));
+INSERT INTO Employe VALUES (3241, 'Caroline', 'Girard', 'MANAGER', TO_DATE('29-05-2014', 'DD-MM-YYYY'), 3900.00, 4, 35, 'caroline.girard@email.com', TO_DATE('22-06-1987', 'DD-MM-YYYY'));
+INSERT INTO Employe VALUES (3242, 'Olivier', 'Lambert', 'POLYVALENT', TO_DATE('03-10-2015', 'DD-MM-YYYY'), 3500.00, 2, 30, 'olivier.lambert@email.com', TO_DATE('14-07-1990', 'DD-MM-YYYY'));
+INSERT INTO Employe VALUES (3243, 'Isabelle', 'Bonnet', 'CHEF', TO_DATE('07-02-2016', 'DD-MM-YYYY'), 4600.00, 5, 40, 'isabelle.bonnet@email.com', TO_DATE('27-04-1985', 'DD-MM-YYYY'));
+INSERT INTO Employe VALUES (3244, 'Julien', 'Dupuis', 'MANAGER', TO_DATE('12-06-2017', 'DD-MM-YYYY'), 4100.00, 3, 35, 'julien.dupuis@email.com', TO_DATE('08-09-1988', 'DD-MM-YYYY'));
+INSERT INTO Employe VALUES (3245, 'Emilie', 'Lopez', 'POLYVALENT', TO_DATE('16-11-2018', 'DD-MM-YYYY'), 3400.00, 1, 28, 'emilie.lopez@email.com', TO_DATE('19-12-1989', 'DD-MM-YYYY'));
+INSERT INTO Employe VALUES (3246, 'Christophe', 'Gauthier', 'CHEF', TO_DATE('20-03-2019', 'DD-MM-YYYY'), 4800.00, 6, 38, 'christophe.gauthier@email.com', TO_DATE('02-03-1986', 'DD-MM-YYYY'));
+
 

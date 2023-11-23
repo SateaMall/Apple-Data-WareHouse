aa6b77a39a4b6127a02dc6914902638a728323c9
Mohamad Satea ALMALLOUHI <75503482+SateaMall@users.noreply.github.com>
2023-11-23 16:10:05 +0100

    Create RemplissageStock.sql

---
+++ RemplissageStock.sql
@@ -0,0 +1,11 @@
+-- Combinaisons pour 'neuf'
+INSERT INTO EtatType (idEtat, etat, idType, type) VALUES (1, 'neuf', 101, 'smartphone');
+INSERT INTO EtatType (idEtat, etat, idType, type) VALUES (2, 'neuf', 102, 'ordinateur');
+INSERT INTO EtatType (idEtat, etat, idType, type) VALUES (3, 'neuf', 103, 'ecouteurs');
+INSERT INTO EtatType (idEtat, etat, idType, type) VALUES (4, 'neuf', 104, 'tablette');
+
+-- Combinaisons pour 'reconditionne'
+INSERT INTO EtatType (idEtat, etat, idType, type) VALUES (5, 'reconditionne', 105, 'smartphone');
+INSERT INTO EtatType (idEtat, etat, idType, type) VALUES (6, 'reconditionne', 106, 'ordinateur');
+INSERT INTO EtatType (idEtat, etat, idType, type) VALUES (7, 'reconditionne', 107, 'ecouteurs');
+INSERT INTO EtatType (idEtat, etat, idType, type) VALUES (8, 'reconditionne', 108, 'tablette');

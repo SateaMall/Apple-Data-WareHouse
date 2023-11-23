b33bdba2cfc3263f5f53ab4b9d7c7922d9dbbd6d
Mohamad Satea ALMALLOUHI <75503482+SateaMall@users.noreply.github.com>
2023-11-23 16:16:28 +0100

    Ajoute Insert Etats

--- RemplissageStock.sql
+++ RemplissageStock.sql
@@ -1,11 +1,13 @@
 -- Combinaisons pour 'neuf'
 INSERT INTO EtatType (idEtat, etat, idType, type) VALUES (1, 'neuf', 101, 'smartphone');
-INSERT INTO EtatType (idEtat, etat, idType, type) VALUES (2, 'neuf', 102, 'ordinateur');
-INSERT INTO EtatType (idEtat, etat, idType, type) VALUES (3, 'neuf', 103, 'ecouteurs');
-INSERT INTO EtatType (idEtat, etat, idType, type) VALUES (4, 'neuf', 104, 'tablette');
+INSERT INTO EtatType (idEtat, etat, idType, type) VALUES (1, 'neuf', 102, 'ordinateur');
+INSERT INTO EtatType (idEtat, etat, idType, type) VALUES (1, 'neuf', 103, 'ecouteurs');
+INSERT INTO EtatType (idEtat, etat, idType, type) VALUES (1, 'neuf', 104, 'tablette');
 
 -- Combinaisons pour 'reconditionne'
-INSERT INTO EtatType (idEtat, etat, idType, type) VALUES (5, 'reconditionne', 105, 'smartphone');
-INSERT INTO EtatType (idEtat, etat, idType, type) VALUES (6, 'reconditionne', 106, 'ordinateur');
-INSERT INTO EtatType (idEtat, etat, idType, type) VALUES (7, 'reconditionne', 107, 'ecouteurs');
-INSERT INTO EtatType (idEtat, etat, idType, type) VALUES (8, 'reconditionne', 108, 'tablette');
+INSERT INTO EtatType (idEtat, etat, idType, type) VALUES (2, 'reconditionne', 101, 'smartphone');
+INSERT INTO EtatType (idEtat, etat, idType, type) VALUES (2, 'reconditionne', 102, 'ordinateur');
+INSERT INTO EtatType (idEtat, etat, idType, type) VALUES (2, 'reconditionne', 103, 'ecouteurs');
+INSERT INTO EtatType (idEtat, etat, idType, type) VALUES (2, 'reconditionne', 104, 'tablette');
+
+

f40e8ae2ab532debb6e88eba6779afb2483c2246
Gürgün Dayıoğlu <gurgun.dayioglu@icloud.com>
2023-11-23 18:07:42 +0100

    add rollup and cube

--- Requetes.sql
+++ Requetes.sql
@@ -76,3 +76,60 @@ GROUP BY
     P.nom
 ORDER BY 
     TotalPurchases DESC;
+
+SELECT 
+    C.departement,
+    V.note,
+    SUM(V.prixHT) AS TotalSalesExclVAT
+FROM 
+    Vente V
+JOIN 
+    Magasin M ON V.siret = M.siret
+JOIN 
+    Client C ON V.idClient = C.idClient
+WHERE 
+    M.pays = 'France'
+GROUP BY 
+    C.departement, 
+    V.note
+ORDER BY 
+    C.departement, 
+    V.note;
+
+SELECT 
+    COALESCE(TO_CHAR(D.annee), 'Toutes les années') AS Annee,
+    COALESCE(TO_CHAR(M.siret), 'Tous les magasins') AS Magasin,
+    SUM(V.prixTTC) AS TotalRevenue,
+    COUNT(*) AS TotalSales
+FROM 
+    Vente V
+JOIN 
+    Magasin M ON V.siret = M.siret
+JOIN 
+    Dates D ON V.idDate = D.idDate
+GROUP BY 
+    ROLLUP(D.annee, M.siret)
+ORDER BY 
+    D.annee, 
+    M.siret;
+
+SELECT 
+    COALESCE(TO_CHAR(D.annee), 'Toutes les années') AS Annee,
+    COALESCE(TO_CHAR(M.siret), 'Touts les magasins') AS Magasin,
+    COALESCE(P.type, 'Tous les types') AS TypeProduit,
+    SUM(V.prixTTC) AS ChiffresAffaires,
+    COUNT(*) AS VentesTotales
+FROM 
+    Vente V
+JOIN 
+    Magasin M ON V.siret = M.siret
+JOIN 
+    Dates D ON V.idDate = D.idDate
+JOIN 
+    Produit P ON V.idProduit = P.idProduit
+GROUP BY 
+    CUBE(D.annee, M.siret, P.type)
+ORDER BY 
+    D.annee, 
+    M.siret, 
+    P.type;
